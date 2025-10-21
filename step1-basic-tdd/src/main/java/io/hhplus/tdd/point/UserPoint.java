e3a5839ccfc3a94992fcabe6bfb0ff5b13faf1fb
jinheelee04 <2pro.jlee@gmail.com>
2025-10-21 21:22:37 +0900

    feat: 최소 충전 금액 미충족시 예외 발생 로직 구현(Green 단계)

--- step1-basic-tdd/src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ step1-basic-tdd/src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -6,11 +6,18 @@ public record UserPoint(
         long updateMillis
 ) {
     public static final long MAX_BALANCE = 100_000L;
+    public static final long MIN_CHARGE_AMOUNT = 100L;
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
     }
 
     public UserPoint addPoints(long amount) {
+        if (amount < MIN_CHARGE_AMOUNT) {
+            throw new IllegalArgumentException(
+                    "충전 금액은 최소 " + MIN_CHARGE_AMOUNT + "원 이상이어야 합니다."
+            );
+        }
+
         long newAmount = this.point + amount;
         if(newAmount > MAX_BALANCE){
             throw new IllegalStateException("최대 잔액은 " + MAX_BALANCE + "원을 초과할 수 없습니다.");
