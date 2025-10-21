b27699557d178f5fd5df514f409a742f20e960d3
jinheelee04 <2pro.jlee@gmail.com>
2025-10-21 21:07:58 +0900

    feat: 충전 후 최대 잔액 초과 예외 발생 로직 구현(Green 단계)

--- step1-basic-tdd/src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ step1-basic-tdd/src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -5,13 +5,17 @@ public record UserPoint(
         long point,
         long updateMillis
 ) {
-
+    public static final long MAX_BALANCE = 100_000L;
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
     }
 
     public UserPoint addPoints(long amount) {
         long newAmount = this.point + amount;
+        if(newAmount > MAX_BALANCE){
+            throw new IllegalStateException("최대 잔액은 " + MAX_BALANCE + "원을 초과할 수 없습니다.");
+        }
         return new UserPoint(this.id, newAmount, System.currentTimeMillis());
     }
+
 }
