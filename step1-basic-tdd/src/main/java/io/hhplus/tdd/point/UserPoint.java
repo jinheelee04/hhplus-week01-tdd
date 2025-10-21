03c6c8077d62195ddd2173924397a9e23b00a5fc
jinheelee04 <2pro.jlee@gmail.com>
2025-10-21 22:04:17 +0900

    feat: 1회 최대 충전금액 초과시 예외 발생 로직 구현(Green 단계)

--- step1-basic-tdd/src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ step1-basic-tdd/src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -7,6 +7,8 @@ public record UserPoint(
 ) {
     public static final long MAX_BALANCE = 100_000L;
     public static final long MIN_CHARGE_AMOUNT = 100L;
+    public static final long MAX_CHARGE_AMOUNT = 50_000L;
+
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
     }
@@ -16,6 +18,10 @@ public record UserPoint(
             throw new IllegalArgumentException(
                     "충전 금액은 최소 " + MIN_CHARGE_AMOUNT + "원 이상이어야 합니다."
             );
+        }
+
+        if (amount > MAX_CHARGE_AMOUNT) {
+            throw new IllegalArgumentException("1회 최대 충전 금액은 " + MAX_CHARGE_AMOUNT + "원입니다");
         }
 
         long newAmount = this.point + amount;
